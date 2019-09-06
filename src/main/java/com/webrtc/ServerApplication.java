8ea92cf340934b7130a4cf02743b4f4f1d407959
2425358736 <2425358736@qq.com>
2019-09-06 10:08:30 +0800

    连接方式

--- src/main/java/com/webrtc/ServerApplication.java
+++ src/main/java/com/webrtc/ServerApplication.java
@@ -1,22 +1,17 @@
 package com.webrtc;
 
-import com.sun.deploy.net.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * <p>
