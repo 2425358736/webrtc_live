be4504be0326e1af086ce54d453e14afb8e37273
2425358736 <2425358736@qq.com>
2019-09-06 09:54:30 +0800

    连接方式

--- src/main/java/com/webrtc/ServerApplication.java
+++ src/main/java/com/webrtc/ServerApplication.java
@@ -1,12 +1,20 @@
 package com.webrtc;
 
+import com.sun.deploy.net.HttpResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,6 +26,7 @@ import java.util.regex.Pattern;
  * @author 刘志强
  * @created Create Time: 2019/3/26
  */
+@RestController
 @SpringBootApplication
 @EnableAutoConfiguration
 public class ServerApplication implements CommandLineRunner {
@@ -30,5 +39,10 @@ public class ServerApplication implements CommandLineRunner {
     @Override
     public void run(String... strings) throws Exception {
         logger.info("服务器启动成功");
+    }
+
+    @GetMapping("/")
+    public void index(HttpServletResponse response, HttpServletRequest request) throws IOException {
+        response.sendRedirect("/web/webBroadcast");
     }
 }
